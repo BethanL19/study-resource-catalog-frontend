715af8380b77debb4496f811607382f0945bffbf
TomaszLaboj <tomasz.laboj@gmail.com>
2023-09-12 14:42:29 +0100

    data rendered using props and map

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
-import { ResourceComponent } from "./RescourceComponent";
+import { Resource, ResourceComponent } from "./RescourceComponent";
+import resources from "../resources.json";
 
 function App() {
+    const resourcesFromJsonFile: Resource[] = resources.resources;
+
     return (
         <div className="App">
-            <ResourceComponent />
+            {resourcesFromJsonFile.map((el) => {
+                return <ResourceComponent key={el.id} resource={el} />;
+            })}
         </div>
     );
 }

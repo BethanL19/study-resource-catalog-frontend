c9a90547eca1b54f61e277f40454e836a36a3ac3
Beth <bethan.hp@gmail.com>
2023-09-13 16:10:39 +0100

    make pretty

--- src/utils/getResources.ts
+++ src/utils/getResources.ts
@@ -2,8 +2,9 @@ import { Resource } from "../components/Resource";
 import { baseURL } from "../config";
 import axios from "axios";
 
-
-export async function getResources(setResources: React.Dispatch<React.SetStateAction<Resource[]>>) {
+export async function getResources(
+    setResources: React.Dispatch<React.SetStateAction<Resource[]>>
+) {
     const response = await axios.get(`${baseURL}/resources`);
-    setResources(response.data)
+    setResources(response.data);
 }

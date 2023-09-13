f2b3fb969dae60d09197df07c519ca74d4ab95b3
TomaszLaboj <tomasz.laboj@gmail.com>
2023-09-13 11:11:26 +0100

    added 8 states to addresoursecomponent

--- src/components/AddResourceComponent.tsx
+++ src/components/AddResourceComponent.tsx
@@ -12,10 +12,19 @@ import {
     Input,
     ModalFooter,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 export function AddResourceComponent(): JSX.Element {
     const { isOpen, onOpen, onClose } = useDisclosure();
-
+    const [resourceName, setResourceName] = useState<string>("");
+    const [authorName, setAuthorName] = useState<string>("");
+    const [url, setUrl] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [contentType, setContentType] = useState<string>("");
+    const [buildPhase, setBuildPhase] = useState<string>("");
+    const [comment, setComment] = useState<string>("");
+    const [reason, setReason] = useState<string>("");
+    console.log(resourceName, authorName);
     return (
         <>
             <Button onClick={onOpen} colorScheme="green">
@@ -30,42 +39,80 @@ export function AddResourceComponent(): JSX.Element {
                     <ModalBody pb={6}>
                         <FormControl>
                             <FormLabel>Resource name</FormLabel>
-                            <Input placeholder="Resource name" />
+                            <Input
+                                placeholder="Resource name"
+                                onChange={(event) =>
+                                    setResourceName(event.target.value)
+                                }
+                            />
                         </FormControl>
 
                         <FormControl>
                             <FormLabel>Author name</FormLabel>
-                            <Input placeholder="Author name" />
+                            <Input
+                                placeholder="Author name"
+                                onChange={(event) =>
+                                    setAuthorName(event.target.value)
+                                }
+                            />
                         </FormControl>
 
                         <FormControl>
                             <FormLabel>URL</FormLabel>
-                            <Input placeholder="URL" />
+                            <Input
+                                placeholder="URL"
+                                onChange={(event) => setUrl(event.target.value)}
+                            />
                         </FormControl>
 
                         <FormControl>
                             <FormLabel>Description</FormLabel>
-                            <Input placeholder="Description" />
+                            <Input
+                                placeholder="Description"
+                                onChange={(event) =>
+                                    setDescription(event.target.value)
+                                }
+                            />
                         </FormControl>
 
                         <FormControl>
                             <FormLabel>Content type</FormLabel>
-                            <Input placeholder="Content type" />
+                            <Input
+                                placeholder="Content type"
+                                onChange={(event) =>
+                                    setContentType(event.target.value)
+                                }
+                            />
                         </FormControl>
 
                         <FormControl>
                             <FormLabel>Build phase</FormLabel>
-                            <Input placeholder="Build phase" />
+                            <Input
+                                placeholder="Build phase"
+                                onChange={(event) =>
+                                    setBuildPhase(event.target.value)
+                                }
+                            />
                         </FormControl>
 
                         <FormControl>
                             <FormLabel>Comment</FormLabel>
-                            <Input placeholder="Comment" />
+                            <Input
+                                placeholder="Comment"
+                                onChange={(event) =>
+                                    setComment(event.target.value)
+                                }
+                            />
                         </FormControl>
 
                         <FormControl>
                             <FormLabel>Reason for recommending</FormLabel>
-                            <Input placeholder="Reason" />
+                            <Input
+                                placeholder="Reason"
+                                onChange={(event) =>
+                                    setReason(event.target.value)
+                                }
+                            />
                         </FormControl>
                     </ModalBody>
 

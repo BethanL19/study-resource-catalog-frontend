ea649d317ab8cfcebf730ed9c2bcc12f443e577a
oskartyrala <oskartyrala@protonmail.com>
2023-09-13 12:57:10 +0200

    Implement adding resources

--- src/components/AddResourceComponent.tsx
+++ src/components/AddResourceComponent.tsx
@@ -1,18 +1,21 @@
 import {
-    useDisclosure,
     Button,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalCloseButton,
-    ModalBody,
     FormControl,
     FormLabel,
     Input,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
     ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    useDisclosure,
+    useToast,
 } from "@chakra-ui/react";
+import axios from "axios";
 import { useState } from "react";
+import { baseURL } from "../config";
 
 export function AddResourceComponent(): JSX.Element {
     const { isOpen, onOpen, onClose } = useDisclosure();
@@ -25,6 +28,37 @@ export function AddResourceComponent(): JSX.Element {
     const [comment, setComment] = useState<string>("");
     const [reason, setReason] = useState<string>("");
     console.log(resourceName, authorName);
+
+    const toast = useToast();
+
+    const handleSubmit = async () => {
+        try {
+            const response = await axios.post(`${baseURL}/resources/new`, {
+                resource_name: resourceName,
+                author_name: authorName,
+                url,
+                description,
+                content_type: contentType,
+                build_phase: buildPhase,
+                recommender_id: 1,
+                recommender_comment: comment,
+                recommender_reason: reason,
+            });
+            console.log(response.data);
+            onClose();
+
+            toast({
+                title: "Resource added.",
+                description: "Your resource has been submitted.",
+                status: "success",
+                duration: 2000,
+                isClosable: true,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <>
             <Button onClick={onOpen} colorScheme="green">
@@ -41,6 +75,7 @@ export function AddResourceComponent(): JSX.Element {
                             <FormLabel>Resource name</FormLabel>
                             <Input
                                 placeholder="Resource name"
+                                value={resourceName}
                                 onChange={(event) =>
                                     setResourceName(event.target.value)
                                 }
@@ -51,6 +86,7 @@ export function AddResourceComponent(): JSX.Element {
                             <FormLabel>Author name</FormLabel>
                             <Input
                                 placeholder="Author name"
+                                value={authorName}
                                 onChange={(event) =>
                                     setAuthorName(event.target.value)
                                 }
@@ -61,6 +97,7 @@ export function AddResourceComponent(): JSX.Element {
                             <FormLabel>URL</FormLabel>
                             <Input
                                 placeholder="URL"
+                                value={url}
                                 onChange={(event) => setUrl(event.target.value)}
                             />
                         </FormControl>
@@ -69,6 +106,7 @@ export function AddResourceComponent(): JSX.Element {
                             <FormLabel>Description</FormLabel>
                             <Input
                                 placeholder="Description"
+                                value={description}
                                 onChange={(event) =>
                                     setDescription(event.target.value)
                                 }
@@ -79,6 +117,7 @@ export function AddResourceComponent(): JSX.Element {
                             <FormLabel>Content type</FormLabel>
                             <Input
                                 placeholder="Content type"
+                                value={contentType}
                                 onChange={(event) =>
                                     setContentType(event.target.value)
                                 }
@@ -89,6 +128,7 @@ export function AddResourceComponent(): JSX.Element {
                             <FormLabel>Build phase</FormLabel>
                             <Input
                                 placeholder="Build phase"
+                                value={buildPhase}
                                 onChange={(event) =>
                                     setBuildPhase(event.target.value)
                                 }
@@ -99,6 +139,7 @@ export function AddResourceComponent(): JSX.Element {
                             <FormLabel>Comment</FormLabel>
                             <Input
                                 placeholder="Comment"
+                                value={comment}
                                 onChange={(event) =>
                                     setComment(event.target.value)
                                 }
@@ -109,6 +150,7 @@ export function AddResourceComponent(): JSX.Element {
                             <FormLabel>Reason for recommending</FormLabel>
                             <Input
                                 placeholder="Reason"
+                                value={reason}
                                 onChange={(event) =>
                                     setReason(event.target.value)
                                 }
@@ -117,10 +159,16 @@ export function AddResourceComponent(): JSX.Element {
                     </ModalBody>
 
                     <ModalFooter>
-                        <Button colorScheme="blue" mr={3}>
-                            Save
+                        <Button
+                            onClick={handleSubmit}
+                            colorScheme="green"
+                            mr={3}
+                        >
+                            Submit
                         </Button>
-                        <Button onClick={onClose}>Cancel</Button>
+                        <Button onClick={onClose} colorScheme="red">
+                            Cancel
+                        </Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
